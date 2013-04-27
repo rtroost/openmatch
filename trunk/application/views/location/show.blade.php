863e5b942304cfb99557c6e75ab381be63938b70
troost.r@gmail.com <troost.r@gmail.com@ad2b0116-d57f-d6e1-df79-10655b236bf2>
2013-04-27 10:58:55 +0000

    Google Maps toegevoegd voor elke locatie pagina en een begin gemaakt aan het beoordelingssysteem

--- trunk/application/views/location/show.blade.php
+++ trunk/application/views/location/show.blade.php
@@ -12,29 +12,114 @@
 
 	<div class="container">
 
-		<div class="row-fluid">
+		<div class="location">
 
-			<div class="span12">
+			<div class="row-fluid">
 
-				<h3>{{ $location->name }}</h3>
-				@if( $location->website )
-					<a href="{{ $location->website }}"> Website </a>
-				@endif
+				<div class="span5">
+					<div class="location-map">
+						<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" style="width:100%; height:100%;" src="https://maps.google.nl/maps?q={{$location->latitude}}+,{{$location->longitude}}&amp;output=embed"></iframe>
+					</div>
+				</div><!--/span5-->
 
-				<p>Plaats: {{ $location->city }}</p>
-				<p>Adres: {{ $location->street }} {{ $location->number }}</p>
-				<p>Postcode: {{ $location->postalcode }}</p>
+				<div class="span7">
 
-				<p>Types: 
-					@foreach($location->types as $type)
-						{{ $type->naam }}
-					@endforeach
-				</p>
+					<h3>{{ $location->name }}</h3>
+					<table>
+						@if( $location->website )
+						<tr>
+							<td>Website:</td>
+							<td><a href="{{ $location->website }}">{{ $location->website }}</a></td>
+						</tr>
+						@endif
+						<tr>
+							<td>Adres:</td>
+							<td>{{ $location->street }} {{ $location->number }}</td>
+						</tr>
+						<tr>
+							<td>Postcode:</td>
+							<td>{{ $location->postalcode }}</td>
+						</tr>
+						<tr>
+							<td>Plaats:</td>
+							<td>{{ $location->city }}</td>
+						</tr>
+						<tr>
+							<td>Types:</td>
+							<td>
+								@foreach($location->types as $type)
+									{{ $type->naam }}
+								@endforeach
+							</td>
+						</tr>
+					</table>
+				</div><!--/span8-->
+
+			</div><!--/row-fluid-->
+
+			<div class="row-fluid">
+
+				<hr />
+
+				<h3>Opmerkingen en beoordelingen</h3>
+
+				<div id="comment-container">
+
+					<div class="comment">
+
+						<div class="comment-inner">
+
+							<div class="comment-inner-author">
+								<a href="#">Remco van der Kleijn</a> <small>zei op 16 februari 2013:</small>
+							</div><!--/comment-inner-author-->
+
+							<div class="comment-inner-body">
+								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</p>
+							</div><!--/comment-inner-body-->
+
+						</div><!--/comment-inner-->
+
+						<div class="comment">
+
+							<div class="comment-inner">
+
+								<div class="comment-inner-author">
+									<a href="#">Robbie Troost</a> <small>antwoorde 16 februari 2013 met:</small>
+								</div><!--/comment-inner-author-->
+
+								<div class="comment-inner-body">
+									<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.</p>
+								</div><!--/comment-inner-body-->
+
+							</div><!--/comment-inner-->
+
+						</div><!--/comment-->
+
+					</div><!--/comment-->
+
+				</div><!--/comment-container-->
+
+				<div id="comment_post">
+
+					<h3>Geef je oordeel of discusseer mee!</h3>
+
+					@if( ! Auth::check())
+
+					<div class="comment_post-notLoggedIn">
+						Je moet eerst {{ HTML::link_to_route('login', 'inloggen') }} of {{ HTML::link_to_route('register', 'registreren') }} om hier gebruik van te maken.
+					</div>
+
+					@else
 
 
-			</div><!--/span9-->
 
-		</div><!--/row-fluid-->
+					@endif
+
+				</div>
+
+			</div><!--/row-fluid-->
+
+		</div><!--/location-->
 	</div><!--/container-->
 </div><!--/content-->
 
