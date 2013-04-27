7b02d0257ea911d5f6f33deabe6bed33d65763ab
troost.r@gmail.com <troost.r@gmail.com@ad2b0116-d57f-d6e1-df79-10655b236bf2>
2013-04-27 11:49:50 +0000

    Match veranderd naar Locatie's

--- trunk/application/views/home/index.blade.php
+++ trunk/application/views/home/index.blade.php
@@ -124,7 +124,7 @@
 							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
 						</p>
 						<p>
-							<a href="#" class="btn btn-primary btn-large">Match mij!</a>
+							<a href="#" class="btn btn-large">Match mij!</a>
 						</p>
 					</div><!--/.span6-->
 
