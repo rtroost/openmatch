47d0b28070cb2efb39aea2fe0dd1229a5edd2357
troost.r@gmail.com <troost.r@gmail.com@ad2b0116-d57f-d6e1-df79-10655b236bf2>
2013-05-05 12:48:24 +0000

    Kleine tussen update van events

---
+++ application/views/events/new.blade.php
@@ -0,0 +1,19 @@
+@layout('layouts.default')
+
+@section('content')
+
+<div class="content">
+
+	<div class="container">
+
+		<div class="row-fluid">
+
+
+
+		</div><!--/row-fluid-->
+
+	</div><!--/container-->
+
+</div><!--/content-->
+
+@endsection
