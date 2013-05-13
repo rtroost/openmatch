53f5a09e7ce8f0233e2c360ec8aa339f038f3380
troost.r@gmail.com <troost.r@gmail.com@ad2b0116-d57f-d6e1-df79-10655b236bf2>
2013-05-13 15:54:49 +0000

    Fixes en shiizzles

--- trunk/public/js/main.js
+++ trunk/public/js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 	var path = location.pathname.split("/");
-	//window.BASE = "http://dev.openmatch/";
-	window.BASE = "http://"+ location.hostname + "/" + path[1] + "/" + path[2] + "/";
+	// window.BASE = "http://dev.openmatch/";
+	if(location.hostname.indexOf("127.0.0.1") !== -1 || location.hostname.indexOf("localhost") !== -1){
+		window.BASE = "http://"+ location.hostname + "/" + path[1] + "/" + path[2] + "/";
+	} else {
+		window.BASE = "http://"+ location.hostname + "/";
+	}
 	window.IMGLOC = BASE + "img/";
 });
